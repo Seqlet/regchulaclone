8e0b0a170b927893b70a1573f427cb77d076aba4
Non Eks <ratchanoneks@gmail.com>
2019-12-12 18:28:27 +0700

    feat: add login function

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [TableComponent],
-  imports: [CommonModule],
-  exports: [TableComponent]
+  declarations: [TableComponent, TabsComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [TableComponent, TabsComponent]
 })
 export class CoreModule {}
