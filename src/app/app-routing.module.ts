368f6df9ac93274fc11617e9f2f2a94d46c68847
Non Eks <ratchanoneks@gmail.com>
2019-12-13 11:26:05 +0700

    feat : change color icon onclick

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ActiveTab } from './core/interfaces/enum';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -5,27 +6,50 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    data: {
+      hasTab: false
+    }
   },
   {
     path: 'regis',
-    loadChildren: () => import('./pages/regis/regis.module').then( m => m.RegisPageModule)
+    loadChildren: () => import('./pages/regis/regis.module').then( m => m.RegisPageModule),
+    data: {
+      hasTab: true,
+      activeTab: ActiveTab.regis
+    }
   },
   {
     path: 'regisout',
-    loadChildren: () => import('./pages/regisout/regisout.module').then( m => m.RegisoutPageModule)
+    loadChildren: () => import('./pages/regisout/regisout.module').then( m => m.RegisoutPageModule),
+    data: {
+      hasTab: true,
+      activeTab: ActiveTab.regisOutcome
+    }
   },
   {
     path: 'grade',
-    loadChildren: () => import('./pages/grade/grade.module').then( m => m.GradePageModule)
+    loadChildren: () => import('./pages/grade/grade.module').then( m => m.GradePageModule),
+    data: {
+      hasTab: true,
+      activeTab: ActiveTab.grade
+    }
   },
   {
     path: 'withdraw',
-    loadChildren: () => import('./pages/withdraw/withdraw.module').then( m => m.WithdrawPageModule)
+    loadChildren: () => import('./pages/withdraw/withdraw.module').then( m => m.WithdrawPageModule),
+    data: {
+      hasTab: true,
+      activeTab: ActiveTab.withdraw
+    }
   },
   {
     path: 'stdinfo',
-    loadChildren: () => import('./pages/stdinfo/stdinfo.module').then( m => m.StdinfoPageModule)
+    loadChildren: () => import('./pages/stdinfo/stdinfo.module').then( m => m.StdinfoPageModule),
+    data: {
+      hasTab: true,
+      activeTab: ActiveTab.studentInfo
+    }
   },
 ];
 
--- src/app/core/components/tabs/tabs.component.ts
+++ src/app/core/components/tabs/tabs.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ActiveTab } from "./../../interfaces/enum";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.scss'],
+  selector: "app-tabs",
+  templateUrl: "./tabs.component.html",
+  styleUrls: ["./tabs.component.scss"]
 })
 export class TabsComponent implements OnInit {
+  @Input() activeTab: ActiveTab;
 
-  constructor() { }
+  constructor() {}
+
+  get ActiveTab() {
+    console.log(this.activeTab, 'ac')
+    return ActiveTab;
+  }
 
   ngOnInit() {}
-
 }
